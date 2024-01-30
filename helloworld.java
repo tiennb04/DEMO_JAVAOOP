6dcd7e1d620428d970e10d61a232ab82e2c522e9
tiennb04 <156460526+tiennb04@users.noreply.github.com>
2024-01-30 15:18:01 +0700

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+        // In "Hello World" trên terminal window.
+        System.out.println("Hello World");
+    }
+
+}
+
