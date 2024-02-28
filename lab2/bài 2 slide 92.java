1580600e1cb8bc270f5769e4f986c91e077cb684
tiennb04 <156460526+tiennb04@users.noreply.github.com>
2024-02-28 12:41:46 +0700

    bài2slide92

---
+++ lab2/bài 2 slide 92.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class bài2slide92 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("nhap so bat ky:");
+        a= sc.nextInt();
+        if(a % 2 == 1){
+            System.out.println("vay"+a +"la so le");
+        }
+        else{
+            System.out.println(a +"la so chan");
+        }
+        }
+        
+    }
